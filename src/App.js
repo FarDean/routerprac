db0eafc378f370495883dcb52409ce77a719bfce
FarDean <sfh1212@gmail.com>
2021-03-11 12:56:52 +0330

    before auth0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Protected from "./components/Protected";
+import Home from "./components/Home";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>kos</h1>
+			<Router>
+				<Link to="/protected">protectd</Link>
+				<Link to="/">Home</Link>
+				<Switch>
+					<Route path="/protected">
+						<Protected />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
